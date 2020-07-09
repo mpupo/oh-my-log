f3adba97c5ffd399985a8376929bc4dc8d44b02a
Murilo Pupo de Oliveira <48927673+mpupo@users.noreply.github.com>
2020-07-09 16:48:16 -0300

    Criado o User model

--- oh_my_log/log_api/models.py
+++ oh_my_log/log_api/models.py
@@ -1,8 +1,12 @@
 from django.db import models
-
+from django.core import validators
 # Create your models here.
 class User(models.Model):
-    pass
+    "User model"
+    name = models.CharField('Name', max_length=50)
+    email = models.EmailField('E-mail', max_length=254, validators=[validators.EmailValidator()])
+    password = models.CharField('Password', max_length=50, validators=[validators.MinLengthValidator(8)])
+    last_login = models.DateTimeField('Last login', auto_now_add=True)
 
 class Group(models.Model):
     pass
