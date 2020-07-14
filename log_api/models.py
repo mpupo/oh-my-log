3b5af03b417b59b5847b92f66f3d7db84faf0705
Murilo Pupo de Oliveira <48927673+mpupo@users.noreply.github.com>
2020-07-13 23:43:13 -0300

    Corrigida formatação do arquivo e execução do DELETE no execution do Event model

--- log_api/models.py
+++ log_api/models.py
@@ -1,30 +1,37 @@
 from django.db import models
 from django.core import validators
+
 # Create your models here.
 class User(models.Model):
     "User model"
-    name = models.CharField('Name', max_length=50)
-    email = models.EmailField('E-mail', max_length=254, validators=[validators.EmailValidator()])
-    password = models.CharField('Password', max_length=50, validators=[validators.MinLengthValidator(8)])
-    last_login = models.DateTimeField('Last login', auto_now_add=True)
+    name = models.CharField("Name", max_length=50)
+    email = models.EmailField(
+        "E-mail", max_length=254, validators=[validators.EmailValidator()]
+    )
+    password = models.CharField(
+        "Password", max_length=50, validators=[validators.MinLengthValidator(8)]
+    )
+    last_login = models.DateTimeField("Last login", auto_now_add=True)
+
 
 class Group(models.Model):
     pass
 
+
 class Application(models.Model):
     "Application model"
 
-    name = models.CharField('Name', max_length=50, null=False, default='App-Name')
-    active = models.BooleanField('Active', null=False, default=True)
-    description = models.TextField('Description', null=True)
-    version = models.CharField('Version', max_length=8, null=True)
+    name = models.CharField("Name", max_length=50, null=False, default="App-Name")
+    active = models.BooleanField("Active", null=False, default=True)
+    description = models.TextField("Description", null=True)
+    version = models.CharField("Version", max_length=8, null=True)
 
 
 class OperationSystem(models.Model):
     "Operation systems model"
 
-    name = models.CharField('Name', max_length=50)
-    version = models.CharField('Version', max_length=8)
+    name = models.CharField("Name", max_length=50)
+    version = models.CharField("Version", max_length=8)
     installed_apps = models.ManyToManyField(Application)
 
 
@@ -33,39 +40,48 @@ class Machine(models.Model):
 
     class MachineEnvChoices(models.TextChoices):
         "A Class to choose a variety of environment types"
-        DEV = 'DEV', 'Development'
-        PROD = 'PROD', 'Production'
-        QA = 'QA', 'Quality Assurance'
-        TEST = 'TEST', 'Test'
+        DEV = "DEV", "Development"
+        PROD = "PROD", "Production"
+        QA = "QA", "Quality Assurance"
+        TEST = "TEST", "Test"
 
-    name = models.CharField('Name', max_length=50, null=True)
-    active = models.BooleanField('Active', null=False, default=True)
-    environment = models.CharField('Enviroment', max_length=30, choices=MachineEnvChoices.choices, default=MachineEnvChoices.DEV)
-    address = models.GenericIPAddressField(protocol='IPV4', validators=[validators.validate_ipv4_address], null=True)
+    name = models.CharField("Name", max_length=50, null=True)
+    active = models.BooleanField("Active", null=False, default=True)
+    environment = models.CharField(
+        "Enviroment",
+        max_length=30,
+        choices=MachineEnvChoices.choices,
+        default=MachineEnvChoices.DEV,
+    )
+    address = models.GenericIPAddressField(
+        protocol="IPV4", validators=[validators.validate_ipv4_address], null=True
+    )
     operation_systems = models.ManyToManyField(OperationSystem)
 
 
 class Execution(models.Model):
     pass
 
+
 class Event(models.Model):
     "Event model"
 
     class EventLevelChoices(models.TextChoices):
         "A Class to choose a variety of event types"
-        CRITICAL = 'CRITICAL', 'CRITICAL'
-        DEBUG = 'DEBUG', 'DEBUG'
-        WARNING = 'WARNING', 'WARNING'
-        INFO = 'INFO', 'INFO'
+        CRITICAL = "CRITICAL", "CRITICAL"
+        DEBUG = "DEBUG", "DEBUG"
+        WARNING = "WARNING", "WARNING"
+        INFO = "INFO", "INFO"
 
     level = models.CharField(
-        'Level', max_length=20,
+        "Level",
+        max_length=20,
         choices=EventLevelChoices.choices,
-        default=EventLevelChoices.INFO)
-    dateref = models.DateField('Date', auto_now_add=True)
-    archived = models.BooleanField('Archived')
-    description = models.TextField('Description')
+        default=EventLevelChoices.INFO,
+    )
+    dateref = models.DateField("Date", auto_now_add=True)
+    archived = models.BooleanField("Archived")
+    description = models.TextField("Description")
     execution = models.ForeignKey(
-        Execution,
-        on_delete=models.deletion.DO_NOTHING,
-        related_name='execution')
+        Execution, on_delete=models.deletion.CASCADE, related_name="execution"
+    )
